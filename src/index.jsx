13af4b0a82f34cb05e3f71aa313ca9395b2a21f1
XAITYH <petrovskijpavel1@gmail.com>
2024-08-26 02:27:33 +0300

    Ready card styles

--- src/index.jsx
+++ src/index.jsx
@@ -11,25 +11,31 @@ export function Card(props) {
     <div class="card">
       <div class="new">{props.new}</div>
       <img class="shopExampleImg" src={props.img_src} alt="shop example" />
-      <button class="quickView">
-        <span class="quickViewSpan">
-          <img src="public/fullscreen.svg" alt="quick view" /> Quick View
-        </span>
-      </button>
-      <h3 class="shopExHeader">{props.header}</h3>
-      <p class="shopExDescr">Same manufacturer as</p>
-      <p class="author">{props.author}</p>
-      <img src="public/dot.svg" alt="" />
-      <p class="bvlgari">{props.bvlgari}</p>
-      <p class="cost">
-        <img src="public/$.svg" alt="dollars" />
-        {props.cost}
-      </p>
+      <div class="inCardContainer">
+        <button class="quickView">
+          <span class="quickViewSpan">
+            <img src="public/fullscreen.svg" alt="quick view" /> Quick View
+          </span>
+        </button>
+        <h3 class="shopExHeader">{props.header}</h3>
+        <p class="shopExDescr">Same manufacturer as</p>
+        <div class="authorAndBvlgariContainer">
+          <p class="author">{props.author}</p>
+          <img class="dot" src="public/dot.svg" alt="" />
+          <p class="bvlgari">{props.bvlgari}</p>
+        </div>
+        <div class="costAndColorsCont">
+          <p class="cost">
+            <img src="public/$.svg" alt="dollars" />
+            {props.cost}
+          </p>
+          <p class="colors">{props.num_colors} COLORS</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-// render(<Card new={_new} img_src={imgShopEx + 1} header="14k Solid Gold Diamond Drop Chain Earrings" author={vanCleef} bvlgari={_bvlgari} cost="315"/>, document.getElementById("arrivals_card_preact"));
 if (typeof window !== "undefined") {
   hydrateRoot(
     document.querySelector(".arrivals_card_preact"),
@@ -40,6 +46,7 @@ if (typeof window !== "undefined") {
       author={vanCleef}
       bvlgari={_bvlgari}
       cost="315"
+      num_colors="2"
     />
   );
 }
