5b7a6eebf6c240f43ad23fdd70c49eef6af8d50d
XAITYH <petrovskijpavel1@gmail.com>
2024-08-28 23:40:49 +0300

    Changed styles to be like in figma layout

--- src/index.jsx
+++ src/index.jsx
@@ -12,24 +12,26 @@ export function Card(props) {
       <div class="new">{props.new}</div>
       <img class="shopExampleImg" src={props.img_src} alt="shop example" />
       <div class="inCardContainer">
-        <button class="quickView">
-          <span class="quickViewSpan">
-            <img src="public/fullscreen.svg" alt="quick view" /> Quick View
-          </span>
-        </button>
-        <h3 class="shopExHeader">{props.header}</h3>
-        <p class="shopExDescr">Same manufacturer as</p>
-        <div class="authorAndBvlgariContainer">
-          <p class="author">{props.author}</p>
-          <img class="dot" src="public/dot.svg" alt="" />
-          <p class="bvlgari">{props.bvlgari}</p>
-        </div>
-        <div class="costAndColorsCont">
-          <p class="cost">
-            <img src="public/$.svg" alt="dollars" />
-            {props.cost}
-          </p>
-          <p class="colors">{props.num_colors} COLORS</p>
+        <div class="inInCardContainer">
+          <button class="quickView">
+            <span class="quickViewSpan">
+              <img class="fullscreenQV" src="public/fullscreen.svg" alt="quick view" /> Quick View
+            </span>
+          </button>
+          <h3 class="shopExHeader">{props.header}</h3>
+          <p class="shopExDescr">Same manufacturer as</p>
+          <div class="authorAndBvlgariContainer">
+            <p class="author">{props.author}</p>
+            <img class="dot" src="public/dot.svg" alt="" />
+            <p class="bvlgari">{props.bvlgari}</p>
+          </div>
+          <div class="costAndColorsCont">
+            <p class="cost">
+              <img src="public/$.svg" alt="dollars" />
+              {props.cost}
+            </p>
+            <p class="colors">{props.num_colors} COLORS</p>
+          </div>
         </div>
       </div>
     </div>
@@ -40,24 +42,24 @@ if (typeof window !== "undefined") {
   hydrateRoot(
     document.querySelector(".arrivals_card_preact"),
     <>
-    <Card
-      new={_new}
-      img_src={imgShopEx + "1.png"}
-      header="14k Solid Gold Diamond Drop Chain Earrings"
-      author={vanCleef}
-      bvlgari={_bvlgari}
-      cost="315"
-      num_colors="2"
-    />
-    <Card
-      new={_new}
-      img_src={imgShopEx + "2.png"}
-      header="14k Solid Gold Interlocking Necklace"
-      author={vanCleef}
-      bvlgari={_bvlgari}
-      cost="350"
-      num_colors="2"
-    />
+      <Card
+        new={_new}
+        img_src={imgShopEx + "1.png"}
+        header="14k Solid Gold Diamond Drop Chain Earrings"
+        author={vanCleef}
+        bvlgari={_bvlgari}
+        cost="315"
+        num_colors="2"
+      />
+      <Card
+        new={_new}
+        img_src={imgShopEx + "2.png"}
+        header="14k Solid Gold Interlocking Necklace"
+        author={vanCleef}
+        bvlgari={_bvlgari}
+        cost="350"
+        num_colors="2"
+      />
     </>
   );
 }
